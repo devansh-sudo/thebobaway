73e70135e22b630954b6ad29cbf7f1bc492099a6
vishnu reclame <vishnu@reclamehub.com>
2023-05-15 14:48:46 +0530

    menu

--- src/components/MenuPage/MenuPage.jsx
+++ src/components/MenuPage/MenuPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-
+import { Image } from 'react-bootstrap';
+// <iframe src="menu.pdf" style={{ width: '100%', height: '2000px' }} />
 const Menu = () => {
   return (
-    <iframe src="menu.pdf" style={{ width: '100%', height: '500px' }} />
+    
+<div> 
+  <Image src="menu_page-0001.jpg" fluid />
+  <Image src="menu_page-0002.jpg" fluid />
+</div>
+
   );
 }
 
